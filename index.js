58463befd3f55b347b80a6838e72aac8e9d54527
Joanna Hughes <joannahughes@hotmail.co.uk>
2020-12-15 11:43:50 +0000

    commit to main

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const calculateBill = (price, vat, tip) => {
 
+    if (typeOf (price, vat, tip) === "number") {
+        let amount = price + (price * vat / 100) + tip;
+        return "£" + amount;
+    } else {
+        return "error"
+    }
 };
 
-module.exports = calculateBill;
+module.exports = calculateBill; 
